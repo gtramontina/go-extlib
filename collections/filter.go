6a36b76899c09b167285f47984b921da1f114ecb
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2021-10-09 16:44:07 +1100

    feat(collections): add filter function

---
+++ collections/filter.go
@@ -0,0 +1,15 @@
+package collections
+
+// Filter calls the provided predicate function once for each element in the
+// given collection slice, and constructs a new slice of all the elements for
+// which the predicate returns true.
+func Filter[Type any](collection []Type, predicate func(Type) bool) []Type {
+	filtered := []Type{}
+	for _, element := range collection {
+		if predicate(element) {
+			filtered = append(filtered, element)
+		}
+	}
+
+	return filtered
+}
---
+++ collections/filter_test.go
@@ -0,0 +1,52 @@
+package collections_test
+
+import (
+	"github.com/gtramontina/go-collections/collections"
+	"github.com/gtramontina/go-collections/internal/assert"
+	"testing"
+)
+
+func TestFilter(t *testing.T) {
+	t.Run("empty collection yields an empty collection", func(t *testing.T) {
+		dummy := func(int) bool { return true }
+		assert.DeepEqual(t, collections.Filter([]int{}, dummy), []int{})
+	})
+
+	t.Run("filters all out", func(t *testing.T) {
+		allOut := func(int) bool { return false }
+		assert.DeepEqual(t, collections.Filter([]int{1}, allOut), []int{})
+		assert.DeepEqual(t, collections.Filter([]int{1, 2}, allOut), []int{})
+	})
+
+	t.Run("filters all in", func(t *testing.T) {
+		allIn := func(int) bool { return true }
+		assert.DeepEqual(t, collections.Filter([]int{1}, allIn), []int{1})
+		assert.DeepEqual(t, collections.Filter([]int{1, 2}, allIn), []int{1, 2})
+	})
+
+	t.Run("conditionally filters elements", func(t *testing.T) {
+		even := func(it int) bool { return it%2 == 0 }
+		odd := func(it int) bool { return !even(it) }
+		assert.DeepEqual(t, collections.Filter([]int{1, 2, 3, 4}, even), []int{2, 4})
+		assert.DeepEqual(t, collections.Filter([]int{1, 2, 3, 4}, odd), []int{1, 3})
+	})
+
+	t.Run("handles different types", func(t *testing.T) {
+		notEmpty := func(it string) bool { return len(it) > 0 }
+		assert.DeepEqual(t, collections.Filter([]string{"a", "", "c"}, notEmpty), []string{"a", "c"})
+
+		type person struct {
+			name string
+			age  uint
+		}
+		legalAge := func(it person) bool { return it.age >= 18 }
+		assert.DeepEqual(t, collections.Filter([]person{
+			{"Jane", 21},
+			{"Jack", 15},
+			{"John", 18},
+		}, legalAge), []person{
+			{"Jane", 21},
+			{"John", 18},
+		})
+	})
+}
