8cd73fc3476adc11edc0c2d72c741f9398c0fda1
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2021-12-30 00:11:31 +1100

    docs(collection): document `Every` function

--- collections/every.go
+++ collections/every.go
@@ -1,5 +1,9 @@
 package collections
 
+// Every runs the given predicate function against each element in the
+// collection. If the result of calling the predicate on all elements is `true`,
+// then it returns `true`. If, at least one returns `false`, then it returns
+// `false`.
 func Every[Type any](collection []Type, predicate func(Type) bool) bool {
 	for _, element := range collection {
 		if !predicate(element) {
