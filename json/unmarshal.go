f6eeeace46d36d5b83b7f5bed3a7d0b24c422edf
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2023-02-15 23:41:42 +1100

    docs(json): add godoc to Unmarshal

--- json/unmarshal.go
+++ json/unmarshal.go
@@ -2,6 +2,10 @@ package json
 
 import "encoding/json"
 
+// Unmarshal unmarshals the given JSON string or byte slice into the given type.
+// It delegates to the standard library's json.Unmarshal function, leveraging
+// generics to figure out the type of the output. If the unmarshaling fails,
+// it panics.
 func Unmarshal[Out any, In string | []byte](in In) Out {
 	var out Out
 
